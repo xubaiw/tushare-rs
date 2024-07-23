14a7e1c6679de03d99354ecc7c99453a61765b5e
Xubai Wang <18016038327@189.cn>
2024-07-23 13:42:50 +0800

    add api macro_rules

--- src/tushare.rs
+++ src/tushare.rs
@@ -1,3 +1,8 @@
+use std::collections::HashMap;
+
+use paste::paste;
+use polars::frame::DataFrame;
+
 use crate::builder::*;
 
 /// A struct to hold all tushare calls
@@ -27,4 +32,65 @@ impl Tushare {
     pub fn query_builder(self: &Self, api_name: &str) -> QueryBuilder {
         QueryBuilder::new(self, api_name)
     }
+}
+
+macro_rules! define_api {
+    ($api_name:ident, $doc_link:literal $(, $param:ident)*) => {
+        paste! {
+            #[doc = concat!("Typed query builder for `", stringify!($api_name), "` API. \n\n")]
+            #[doc = concat!("See <", $doc_link, "> for detailed documentation.")]
+            pub struct [<$api_name:camel QueryBuilder>]<'a> {
+                tushare: &'a Tushare,
+                $(
+                    $param: Option<&'a str>,
+                )*
+            }
+
+            impl<'a> [<$api_name:camel QueryBuilder>]<'a> {
+                pub fn new(tushare: &'a Tushare) -> Self {
+                    Self {
+                        tushare,
+                        $($param: None,)*
+                    }
+                }
+
+                $(
+                    #[doc = concat!("Set `", stringify!($param), "` param for `", stringify!($api_name), "` API query.")]
+                    pub fn $param(mut self, $param: &'a str) -> Self {
+                        self.$param = Some($param);
+                        self
+                    }
+                )*
+
+                /// Consume the typed query builder to produce a [`QueryBuilder`].
+                pub fn into_query_builder(self) -> QueryBuilder<'a> {
+                    let mut dict = HashMap::new();
+                    $(
+                        if let Some($param) = self.$param {
+                            dict.insert(stringify!($param).to_string(), $param.to_string());
+                        }
+                    )*
+                    QueryBuilder::new(&self.tushare, stringify!($api_name)).params(dict)
+                }
+
+                /// Directly run the query without setting fields.
+                pub fn query(self) -> Result<DataFrame, TushareError> {
+                    self.into_query_builder().query()
+                }
+            }
+
+            impl Tushare {
+                pub fn $api_name<'a>(&'a self) -> [<$api_name:camel QueryBuilder>]<'a> {
+                    [<$api_name:camel QueryBuilder>]::new(self)
+                }
+            }
+        }
+    };
+}
+
+define_api! {
+    stock_basic,
+    "https://tushare.pro/document/2?doc_id=25",
+    ts_code,
+    list_status
 }
